546bd1d3d89e43784e7bbdb8329d6c3268ea2d0b
Stephen Bates <stephen.bates@pmcs.com>
2015-12-17 14:29:07 -0800

    Whitespace damage correction - using Linux Kernel C style.

--- membash.c
+++ membash.c
@@ -49,15 +49,15 @@ struct membash {
 	size_t        size;
 	size_t        iters;
 	unsigned      seed;
-    unsigned      blockcpy;
-    unsigned      fence;
+	unsigned      blockcpy;
+	unsigned      fence;
 	unsigned      verbose;
 
-    char          *mmap;
-    int           mmapfd;
+	char          *mmap;
+	int           mmapfd;
 
 	size_t                  (* hash)(size_t);
-    int                     (* run)(struct membash *);
+	int                     (* run)(struct membash *);
 
 	struct timeval          start_time;
 	struct timeval          end_time;
@@ -67,9 +67,9 @@ static const struct membash defaults = {
 	.mem        = NULL,
 	.size       = 1024,
 	.iters      = 1,
-    .blockcpy   = 0,
+	.blockcpy   = 0,
 	.seed       = 0,
-    .mmap       = NULL,
+	.mmap       = NULL,
 	.verbose    = 0,
 	.hash       = NULL,
 };
@@ -89,8 +89,8 @@ static const struct argconfig_commandline_options command_line_options[] = {
 	 "number of iterations to loop over"},
 	{"seed",          "NUM", CFG_LONG_SUFFIX, &defaults.seed, required_argument,
 	 "random seed to use for data (set to 0 for auto-gen seed)"},
-    {"mmap",          "MMAP", CFG_STRING, &defaults.mmap, required_argument,
-            "file to mmap"},
+	{"mmap",          "MMAP", CFG_STRING, &defaults.mmap, required_argument,
+	 "file to mmap"},
 	{"fence",         "", CFG_NONE, &defaults.fence, no_argument,
 	 "add a mfence between setup and run"},
 	{"v",             "", CFG_NONE, &defaults.verbose, no_argument, NULL},
@@ -103,28 +103,28 @@ static int setup(struct membash *m)
 {
 	unsigned sum = 0, *ptr;
 
-    if ( m->mmap ){
+	if ( m->mmap ){
 
-        m->mmapfd = open(m->mmap, O_RDWR);
-	if ( m->mmapfd<0 ){
-	  fprintf(stderr,"%s\n",strerror(errno));
-	  exit(errno);
+		m->mmapfd = open(m->mmap, O_RDWR);
+		if ( m->mmapfd<0 ){
+			fprintf(stderr,"%s\n",strerror(errno));
+			exit(errno);
+		}
+		m->mem = mmap(NULL, m->size, PROT_WRITE | PROT_READ,
+			      MAP_SHARED, m->mmapfd, 0);
+		if ( m->mem==MAP_FAILED ){
+			fprintf(stderr,"%s\n",strerror(errno));
+			exit(errno);
+		}
 	}
-        m->mem = mmap(NULL, m->size, PROT_WRITE | PROT_READ,
-                      MAP_SHARED, m->mmapfd, 0);
-	if ( m->mem==MAP_FAILED ){
-	  fprintf(stderr,"%s\n",strerror(errno));
-	  exit(errno);
-	}
-    }
-    else
-        m->mem = malloc(m->size);
+	else
+		m->mem = malloc(m->size);
 
 	if (m->mem == NULL){
 		fprintf(stderr,"could not allocate for mem!\n");
 		exit(1);
 	}
-    ptr = m->mem;
+	ptr = m->mem;
 
 	gettimeofday(&m->start_time, NULL);
 	for (size_t i=0; i<(m->size/sizeof(unsigned))-1; i++) {
@@ -136,7 +136,7 @@ static int setup(struct membash *m)
 	gettimeofday(&m->end_time, NULL);
 	fprintf(stdout, "Wrote           : ");
 	report_transfer_rate(stdout, &m->start_time,
-                         &m->end_time, m->size);
+			     &m->end_time, m->size);
 	fprintf(stdout, "\n");
 
 	return 0;
@@ -144,18 +144,18 @@ static int setup(struct membash *m)
 
 static int run_memcpy(struct membash *m)
 {
-    void *dest;
+	void *dest;
 
-    dest = malloc(m->size);
+	dest = malloc(m->size);
 	if (dest == NULL){
 		fprintf(stderr,"could not allocate for dest!\n");
 		exit(1);
 	}
 
-    gettimeofday(&m->start_time, NULL);
+	gettimeofday(&m->start_time, NULL);
 	for (size_t iters=0; iters < m->iters; iters++)
 	{
-        memcpy(dest, m->mem, m->size);
+		memcpy(dest, m->mem, m->size);
 	}
 	gettimeofday(&m->end_time, NULL);
 	fprintf(stdout, "Read (memcpy)   : ");
@@ -164,15 +164,15 @@ static int run_memcpy(struct membash *m)
 			     m->iters*m->size);
 	fprintf(stdout, "\n");
 
-    free(dest);
-    return 0;
+	free(dest);
+	return 0;
 }
 
 static int run_dumb(struct membash *m)
 {
-    unsigned sum = 0, *ptr;
+	unsigned sum = 0, *ptr;
 
-    ptr = m->mem;
+	ptr = m->mem;
 
 	gettimeofday(&m->start_time, NULL);
 	for (size_t iters=0; iters < m->iters; iters++)
@@ -189,7 +189,7 @@ static int run_dumb(struct membash *m)
 			fprintf(stderr,"sum did not add to zero (%u)!\n",
 				sum);
 			exit(1);
-            }
+		}
 	}
 	gettimeofday(&m->end_time, NULL);
 	fprintf(stdout, "Read (dumb)     : ");
@@ -203,35 +203,35 @@ static int run_dumb(struct membash *m)
 
 static int run_blockcpy(struct membash *m)
 {
-    typedef struct { char a[m->blockcpy]; } membash_t;
-    membash_t dst, *ptr;
+	typedef struct { char a[m->blockcpy]; } membash_t;
+	membash_t dst, *ptr;
 
 	gettimeofday(&m->start_time, NULL);
 	for (size_t iters=0; iters < m->iters; iters++){
-        ptr = m->mem;
+		ptr = m->mem;
 		for (size_t i=0; i<(m->size/sizeof(membash_t)); i++){
-            dst = *ptr;
-            ptr++;
-        }
-    }
+			dst = *ptr;
+			ptr++;
+		}
+	}
 	gettimeofday(&m->end_time, NULL);
 	fprintf(stdout, "Read (blockcpy) : ");
 	report_transfer_rate(stdout, &m->start_time,
 			     &m->end_time,
 			     m->iters*m->size);
 	fprintf(stdout, "\n");
-    (void) dst; //suppress set but not used warning
+	(void) dst; //suppress set but not used warning
 	return 0;
 }
 
 static void cleanup(struct membash *m)
 {
-    if ( m->mmap ){
-        munmap(m->mem, m->size);
-        close(m->mmapfd);
-    }
-    else
-        free(m->mem);
+	if ( m->mmap ){
+		munmap(m->mem, m->size);
+		close(m->mmapfd);
+	}
+	else
+		free(m->mem);
 }
 
 static inline size_t hash_stupid(size_t i)
@@ -241,7 +241,7 @@ static inline size_t hash_stupid(size_t i)
 
 static inline size_t hash_fast(size_t i)
 {
-    return i;
+	return i;
 }
 
 int main(int argc, char **argv)
@@ -262,32 +262,32 @@ int main(int argc, char **argv)
 	setup(&cfg);
 
 #ifndef __powerpc64__
-    if ( cfg.fence )
-        asm volatile("mfence" ::: "memory");
+	if ( cfg.fence )
+		asm volatile("mfence" ::: "memory");
 #endif
 	cfg.run  = run_dumb;
-    cfg.run(&cfg);
+	cfg.run(&cfg);
 	cfg.hash = hash_stupid;
-    cfg.run(&cfg);
+	cfg.run(&cfg);
 	cfg.hash = hash_fast;
 	cfg.run(&cfg);
 
 	cfg.run  = run_memcpy;
-    cfg.run(&cfg);
+	cfg.run(&cfg);
 	cfg.hash = hash_stupid;
-    cfg.run(&cfg);
+	cfg.run(&cfg);
 	cfg.hash = hash_fast;
 	cfg.run(&cfg);
 
 	if ( cfg.blockcpy ){
-        cfg.run  = run_blockcpy;
-        cfg.run(&cfg);
-        cfg.hash = hash_stupid;
-        cfg.run(&cfg);
-        cfg.hash = hash_fast;
-        cfg.run(&cfg);
-    }
+		cfg.run  = run_blockcpy;
+		cfg.run(&cfg);
+		cfg.hash = hash_stupid;
+		cfg.run(&cfg);
+		cfg.hash = hash_fast;
+		cfg.run(&cfg);
+	}
 
-    cleanup(&cfg);
+	cleanup(&cfg);
 	return 0;
 }
