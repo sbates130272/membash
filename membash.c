e206e4db25ba4a10b58c7bc4828ce413e1249b27
Stephen Bates <stephen.bates@pmcs.com>
2015-12-16 17:08:39 -0700

    Improved membash functionality
    Added code to do memcpy testing as well as dumb copying and adding.

--- membash.c
+++ membash.c
@@ -44,9 +44,12 @@ struct membash {
 	size_t        size;
 	size_t        iters;
 	unsigned      seed;
+    unsigned      fence;
 	unsigned      verbose;
 
 	size_t                  (* hash)(size_t);
+    int                     (* run)(struct membash *);
+
 	struct timeval          start_time;
 	struct timeval          end_time;
 };
@@ -72,6 +75,8 @@ static const struct argconfig_commandline_options command_line_options[] = {
 	 "number of iterations to loop over"},
 	{"seed",          "NUM", CFG_LONG_SUFFIX, &defaults.seed, required_argument,
 	 "random seed to use for data (set to 0 for auto-gen seed)"},
+	{"fence",         "", CFG_NONE, &defaults.fence, no_argument,
+	 "add a mfence between setup and run"},
 	{"v",             "", CFG_NONE, &defaults.verbose, no_argument, NULL},
 	{"verbose",       "", CFG_NONE, &defaults.verbose, no_argument,
 	 "be verbose"},
@@ -88,7 +93,6 @@ static int setup(struct membash *m)
 		exit(1);
 	}
 
-
 	gettimeofday(&m->start_time, NULL);
 	for (size_t i=0; i<m->size-1; i++) {
 		m->mem[i] = (char)rand();
@@ -97,7 +101,7 @@ static int setup(struct membash *m)
 	m->mem[m->size-1] = UINT_MAX - sum + 1;
 
 	gettimeofday(&m->end_time, NULL);
-	fprintf(stdout, "Wrote: ");
+	fprintf(stdout, "Wrote         : ");
 	report_transfer_rate(stdout, &m->start_time,
 			     &m->end_time, m->size*sizeof(unsigned));
 	fprintf(stdout, "\n");
@@ -105,13 +109,40 @@ static int setup(struct membash *m)
 	return 0;
 }
 
-static int run(struct membash *m)
+static int run_memcpy(struct membash *m)
 {
+    unsigned *dest;
+
+    dest = malloc(m->size*sizeof(unsigned));
+	if (dest == NULL){
+		fprintf(stderr,"could not allocate for dest!\n");
+		exit(1);
+	}
+
+    gettimeofday(&m->start_time, NULL);
+	for (size_t iters=0; iters < m->iters; iters++)
+	{
+        memcpy(dest,m->mem,m->size*sizeof(unsigned));
+	}
+	gettimeofday(&m->end_time, NULL);
+	fprintf(stdout, "Read (memcpy) : ");
+	report_transfer_rate(stdout, &m->start_time,
+			     &m->end_time,
+			     m->iters*m->size*sizeof(unsigned));
+	fprintf(stdout, "\n");
+
+    free(dest);
+    return 0;
+}
+
+static int run_dumb(struct membash *m)
+{
+    unsigned sum = 0;
 
 	gettimeofday(&m->start_time, NULL);
 	for (size_t iters=0; iters < m->iters; iters++)
 	{
-		unsigned sum = 0;
+		sum = 0;
 		for (size_t i=0; i<m->size; i++){
 			if ( m->hash )
 				sum += m->mem[m->hash(i)];
@@ -123,10 +154,10 @@ static int run(struct membash *m)
 			fprintf(stderr,"sum did not add to zero (%u)!\n",
 				sum);
 			exit(1);
-		}
+            }
 	}
 	gettimeofday(&m->end_time, NULL);
-	fprintf(stdout, "Read : ");
+	fprintf(stdout, "Read (dumb)   : ");
 	report_transfer_rate(stdout, &m->start_time,
 			     &m->end_time,
 			     m->iters*m->size*sizeof(unsigned));
@@ -135,9 +166,19 @@ static int run(struct membash *m)
 	return 0;
 }
 
+static void cleanup(struct membash *m)
+{
+    free(m->mem);
+}
+
 static inline size_t hash_stupid(size_t i)
 {
 	return i;
+}
+
+static inline size_t hash_fast(size_t i)
+{
+    return i;
 }
 
 int main(int argc, char **argv)
@@ -156,9 +197,23 @@ int main(int argc, char **argv)
 	srand(cfg.seed);
 
 	setup(&cfg);
-	run(&cfg);
-	cfg.hash = hash_stupid;
-	run(&cfg);
+    if ( cfg.fence )
+        asm volatile("mfence" ::: "memory");
 
+	cfg.run  = run_dumb;
+    cfg.run(&cfg);
+	cfg.hash = hash_stupid;
+    cfg.run(&cfg);
+	cfg.hash = hash_fast;
+	cfg.run(&cfg);
+
+	cfg.run  = run_memcpy;
+    cfg.run(&cfg);
+	cfg.hash = hash_stupid;
+    cfg.run(&cfg);
+	cfg.hash = hash_fast;
+	cfg.run(&cfg);
+
+    cleanup(&cfg);
 	return 0;
 }
